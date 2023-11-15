a975a2b21d6f129483722cbf561196abd4835b10
Segaran <blsgrn@tutanota.com>
2023-11-15 15:54:53 +0530

    successfuly create log out

--- app/src/main/java/com/example/thebloomroom_105_53/DashboardActivity.java
+++ app/src/main/java/com/example/thebloomroom_105_53/DashboardActivity.java
@@ -2,7 +2,9 @@ package com.example.thebloomroom_105_53;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+    }
+
+    public void onLogOutClick(View view) {
+        // Handle logout logic here
+        // For example, you might start a new LoginActivity
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+
+        // Finish the current activity to prevent going back to it with the back button
+        finish();
     }
 }
