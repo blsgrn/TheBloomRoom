8abf25fa537c4bc5d8cc4d47ab1bb5523814ad0f
Segaran <blsgrn@tutanota.com>
2023-11-20 18:13:12 +0530

    Add other category buttons

--- app/src/main/java/com/example/thebloomroom_105_53/CategoriesActivity.java
+++ app/src/main/java/com/example/thebloomroom_105_53/CategoriesActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class CategoriesActivity extends AppCompatActivity {
 
-    Button btnSpecial;
+    Button btnSpecial, btnMixed, btnSeasonal, btnOrchid, btnLily, btnRose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +17,11 @@ public class CategoriesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_categories);
 
         btnSpecial = findViewById(R.id.btnSpecial);
+        btnMixed = findViewById(R.id.btnMixed);
+        btnSeasonal = findViewById(R.id.btnSeasonal);
+        btnOrchid = findViewById(R.id.btnOrchid);
+        btnLily = findViewById(R.id.btnLily);
+        btnRose = findViewById(R.id.btnRose);
 
         btnSpecial.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,5 +42,103 @@ public class CategoriesActivity extends AppCompatActivity {
                 overridePendingTransition(0, 0);
             }
         });
+
+        btnMixed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the category associated with the clicked button
+                String selectedCategory = "Mixed"; // Change this to dynamically get the selected category
+
+                // Create an Intent to start the ShopActivity
+                Intent intent = new Intent(v.getContext(), ShopActivity.class);
+
+                // Pass the selected category to the ShopActivity
+                intent.putExtra("selectedCategory", selectedCategory);
+
+                // Start the ShopActivity
+                startActivity(intent);
+
+                // Override the transition animation
+                overridePendingTransition(0, 0);
+            }
+        });
+
+        btnSeasonal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the category associated with the clicked button
+                String selectedCategory = "Seasonal"; // Change this to dynamically get the selected category
+
+                // Create an Intent to start the ShopActivity
+                Intent intent = new Intent(v.getContext(), ShopActivity.class);
+
+                // Pass the selected category to the ShopActivity
+                intent.putExtra("selectedCategory", selectedCategory);
+
+                // Start the ShopActivity
+                startActivity(intent);
+
+                // Override the transition animation
+                overridePendingTransition(0, 0);
+            }
+        });
+        btnOrchid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the category associated with the clicked button
+                String selectedCategory = "Orchid"; // Change this to dynamically get the selected category
+
+                // Create an Intent to start the ShopActivity
+                Intent intent = new Intent(v.getContext(), ShopActivity.class);
+
+                // Pass the selected category to the ShopActivity
+                intent.putExtra("selectedCategory", selectedCategory);
+
+                // Start the ShopActivity
+                startActivity(intent);
+
+                // Override the transition animation
+                overridePendingTransition(0, 0);
+            }
+        });
+        btnLily.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the category associated with the clicked button
+                String selectedCategory = "Lily"; // Change this to dynamically get the selected category
+
+                // Create an Intent to start the ShopActivity
+                Intent intent = new Intent(v.getContext(), ShopActivity.class);
+
+                // Pass the selected category to the ShopActivity
+                intent.putExtra("selectedCategory", selectedCategory);
+
+                // Start the ShopActivity
+                startActivity(intent);
+
+                // Override the transition animation
+                overridePendingTransition(0, 0);
+            }
+        });
+        btnRose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the category associated with the clicked button
+                String selectedCategory = "Rose"; // Change this to dynamically get the selected category
+
+                // Create an Intent to start the ShopActivity
+                Intent intent = new Intent(v.getContext(), ShopActivity.class);
+
+                // Pass the selected category to the ShopActivity
+                intent.putExtra("selectedCategory", selectedCategory);
+
+                // Start the ShopActivity
+                startActivity(intent);
+
+                // Override the transition animation
+                overridePendingTransition(0, 0);
+            }
+        });
+
     }
 }
