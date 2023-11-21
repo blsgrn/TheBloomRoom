e2d0089b9495708742a2880ccc994d55857312a2
Segaran <blsgrn@tutanota.com>
2023-11-21 17:48:25 +0530

    complete the functionalities of the info activity

--- app/src/main/java/com/example/thebloomroom_105_53/InfoActivity.java
+++ app/src/main/java/com/example/thebloomroom_105_53/InfoActivity.java
@@ -5,18 +5,70 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class InfoActivity extends AppCompatActivity {
+    ImageButton btnImgEmail, btnImgPhone, btnFacebook, btnMap;
 BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        btnImgEmail = findViewById(R.id.imageButtonEmail);
+        btnImgPhone = findViewById(R.id.imageButtonPhone);
+        btnFacebook = findViewById(R.id.imageButtonFacebook);
+        btnMap = findViewById(R.id.imageButtonMap);
+
+        btnImgEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+                emailIntent.setData(Uri.parse("mailto:"));
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"hdcse10553@gmail.com"});
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Customer Inquiry");
+                startActivity(emailIntent);
+            }
+        });
+        btnImgPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+                phoneIntent.setData(Uri.parse("tel:" + "0757952581"));
+                startActivity(phoneIntent);
+            }
+
+        });
+        btnFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String facebookUrl = "https://www.facebook.com/ICBTsrilanka";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(facebookUrl));
+                startActivity(intent);
+            }
+        });
+
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String coordinates = "https://goo.gl/maps/ywAqgBhQovC92Su99";
+
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+
+                mapIntent.setData(Uri.parse(coordinates));
+
+                startActivity(mapIntent);
+
+            }
+        });
 
         bottomNavigationView = findViewById(R.id.bottom_navigator);
         bottomNavigationView.setSelectedItemId(R.id.info);
